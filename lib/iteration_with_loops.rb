121d9634fbe337ecf80a6b0a4280e541b8c9c26d
github username <github email address>
2021-07-04 00:07:08 -0400

    worked on interation of nested arrays, learned about instance of method in ruby

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,15 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  result_array = []
+  
+  src.each do |array|
+    array.each do |element|
+      
+      if element.instance_of? String
+        result_array << element
+      end
+    end
+  end
+  result_array.join(" ")
 end
